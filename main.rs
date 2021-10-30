90981af2bcb790fcf4a136f1bae3dbcafced9030
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-31 00:23:58 +0300

    Add multiply_both function

--- main.rs
+++ main.rs
@@ -1,3 +1,9 @@
 fn main() {
-  println!("Hello, world!");
+  let answer = multiply_both(1.1, 2.2);
+
+  println!("1.1 * 2.2 = {}", answer);
+}
+
+fn multiply_both(x: f64, y: f64) -> f64 {
+  return x * y;
 }
