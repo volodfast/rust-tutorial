3c447ff20af2411b9b73e52922fba4f3fba663f5
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-31 02:47:27 +0300

    Add guessing_game

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
+mod guessing_game;
+
 fn main() {
-  let answer = multiply_both(1.1, 2.2);
-
-  println!("1.1 * 2.2 = {}", answer);
-}
-
-fn multiply_both(x: f64, y: f64) -> f64 {
-  return x * y;
+    guessing_game::play();
 }
