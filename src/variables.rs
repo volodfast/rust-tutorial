540b8a2f7796ddcf3e68415221f249b154c171e0
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-31 23:23:16 +0200

    Add variables.rs to show how variables work in rust

---
+++ src/variables.rs
@@ -0,0 +1,20 @@
+pub fn mutability() {
+  let mut x = 5;
+  println!("The value of x is: {}", x);
+  x = 6;
+  println!("The value of x is: {}", x);
+}
+
+pub fn shadowing() {
+  let x = 5;
+
+  let x = x + 1;
+
+  {
+    let x = x * 2;
+
+    println!("The value of x in the inneer scope is: {}", x);
+  }
+
+  println!("The value of x is: {}", x);
+}
