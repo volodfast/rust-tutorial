819cce29fb82b6d2de0c468cac3d3aa25d07a713
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-11-05 05:01:32 +0200

    Explore enum with params in rust

--- src/enums.rs
+++ src/enums.rs
@@ -1,5 +1,7 @@
 pub fn main() {
   enum_example();
+
+  enum_with_params();
 }
 
 fn enum_example() {
@@ -13,4 +15,17 @@ fn enum_example() {
   let six = IpAddrKind::V6;
 
   println!("IPV4: {:#?}, IPV6: {:#?}", four, six);
+}
+
+fn enum_with_params() {
+  #[derive(Debug)]
+  enum IpAddr {
+    V4(String),
+    V6(String),
+  }
+
+  let home = IpAddr::V4(String::from("127.0.0.1"));
+  let loopback = IpAddr::V6(String::from("::1"));
+
+  println!("Home: {:#?}, Loopback: {:#?}", home, loopback);
 }
