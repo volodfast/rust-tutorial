4be888fcf024a6bbe1b86bf7a67a26970e4e87e6
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-11-09 23:06:22 +0200

    Explore vectors in rust

--- src/collections/vectors.rs
+++ src/collections/vectors.rs
@@ -1,4 +1,14 @@
 pub fn main() {
+  vector_introduction();
+
+  reading_vector();
+
+  iterating_over_vector();
+
+  using_enums_with_vectors();
+}
+
+fn vector_introduction() {
   let mut v = Vec::new();
 
   v.push(5);
@@ -7,4 +17,47 @@ pub fn main() {
   v.push(8);
 
   println!("Vector: {:?}", v);
+}
+
+fn reading_vector() {
+  let v = vec![1, 2, 3, 4, 5];
+
+  let third: &i32 = &v[2];
+  println!("The third element is {}", third);
+
+  match v.get(2) {
+    Some(third) => println!("The third element is {}", third),
+    None => println!("There is no third element."),
+  }
+}
+
+fn iterating_over_vector() {
+  let mut v = vec![100, 32, 57];
+
+  for i in &v {
+    println!("{}", i);
+  }
+
+  for i in &mut v {
+    *i += 50;
+  }
+
+  println!("Vector: {:?}", v);
+}
+
+fn using_enums_with_vectors() {
+  #[derive(Debug)]
+  enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+  }
+
+  let row = vec![
+    SpreadsheetCell::Int(3),
+    SpreadsheetCell::Text(String::from("blue")),
+    SpreadsheetCell::Float(10.12),
+  ];
+
+  println!("The spreadsheet row is: {:#?}", row);
 }
