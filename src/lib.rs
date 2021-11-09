be0cddf4e601f9910fd21ce595c22a72312d4583
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-11-09 22:38:54 +0200

    Comment out unused code in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,16 @@ mod front_of_house {
   pub mod hosting {
     pub fn add_to_waitlist() {}
 
-    fn seat_at_table() {}
+    // fn seat_at_table() {}
   }
 
-  mod serving {
-    fn take_order() {}
+  // mod serving {
+  //   fn take_order() {}
 
-    fn serve_order() {}
+  //   fn serve_order() {}
 
-    fn take_payment() {}
-  }
+  //   fn take_payment() {}
+  // }
 }
 
 use crate::front_of_house::hosting;
