8c61e037d21774c9621bf7ef7bfa025adb7a4fd2
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-11-14 23:06:10 +0200

    Exploring traits and generics in rust

--- src/generics_traits_lifetime/generics.rs
+++ src/generics_traits_lifetime/generics.rs
@@ -1,32 +1,32 @@
 pub fn main() {
-  // find_largest_example();
+  find_largest_example();
 
   method_definition_generic();
 }
 
-// fn find_largest_example() {
-//   fn largest<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
+fn find_largest_example() {
+  fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
 
-//     for &item in list {
-//       if item > largest {
-//         largest = item;
-//       }
-//     }
+    for &item in list {
+      if item > largest {
+        largest = item;
+      }
+    }
 
-//     largest
-//   }
+    largest
+  }
 
-//   let number_list = vec![34, 50, 25, 100, 65];
+  let number_list = vec![34, 50, 25, 100, 65];
 
-//   let result = largest(&number_list);
-//   println!("The largest number is {}", result);
+  let result = largest(&number_list);
+  println!("The largest number is {}", result);
 
-//   let char_list = vec!['y', 'm', 'a', 'q'];
+  let char_list = vec!['y', 'm', 'a', 'q'];
 
-//   let result = largest(&char_list);
-//   println!("The largest char is {}", result);
-// }
+  let result = largest(&char_list);
+  println!("The largest char is {}", result);
+}
 
 fn method_definition_generic() {
   struct Point<T> {
@@ -48,7 +48,7 @@ fn method_definition_generic() {
 
   let p = Point { x: 5, y: 10 };
 
-  println!("p.x = {}", p.x);
+  println!("p.x = {}", p.x());
 
   let p = Point {
     x: 10.0f32,
