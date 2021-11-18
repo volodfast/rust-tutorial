31fa584186e151924a0b8fe70314a3379c242cc9
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-11-18 23:22:36 +0200

    Use Cacher struct in generate_workout function

--- src/iterators_and_closures/heavy_calculation.rs
+++ src/iterators_and_closures/heavy_calculation.rs
@@ -40,21 +40,24 @@ where
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-  let expensive_result = |num| {
+  let mut expensive_result = Cacher::new(|num| {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
     num
-  };
+  });
 
   if intensity < 25 {
-    println!("Today, do {} pushups!", expensive_result(intensity));
+    println!("Today, do {} pushups!", expensive_result.value(intensity));
 
-    println!("Next, do {} situps", expensive_result(intensity));
+    println!("Next, do {} situps", expensive_result.value(intensity));
   } else {
     if random_number == 3 {
       println!("Take a break today! Remember to stay hydrated!");
     } else {
-      println!("Today, run for {} minutes!", expensive_result(intensity));
+      println!(
+        "Today, run for {} minutes!",
+        expensive_result.value(intensity)
+      );
     }
   }
 }
