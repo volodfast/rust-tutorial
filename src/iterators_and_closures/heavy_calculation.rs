62db6bb72b6c584874418097f99df0545624a24f
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-11-18 22:59:49 +0200

    Change code to use closure

--- src/iterators_and_closures/heavy_calculation.rs
+++ src/iterators_and_closures/heavy_calculation.rs
@@ -8,31 +8,22 @@ pub fn main() {
   generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-  println!("calculating slowly...");
-  thread::sleep(Duration::from_secs(2));
-  intensity
-}
-
 fn generate_workout(intensity: u32, random_number: u32) {
-  if intensity < 25 {
-    println!(
-      "Today, do {} pushups!",
-      simulated_expensive_calculation(intensity)
-    );
+  let expensive_result = |num| {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    num
+  };
 
-    println!(
-      "Next, do {} situps",
-      simulated_expensive_calculation(intensity)
-    );
+  if intensity < 25 {
+    println!("Today, do {} pushups!", expensive_result(intensity));
+
+    println!("Next, do {} situps", expensive_result(intensity));
   } else {
     if random_number == 3 {
       println!("Take a break today! Remember to stay hydrated!");
     } else {
-      println!(
-        "Today, run for {} minutes!",
-        simulated_expensive_calculation(intensity)
-      );
+      println!("Today, run for {} minutes!", expensive_result(intensity));
     }
   }
 }
