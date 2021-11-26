8851e6a013121e500de8bcf915ecd6ca2b5342b4
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-11-27 00:06:35 +0200

    Explore multiple owners of mutable data pattern in rust

--- src/smart_pointers/interior_mutability.rs
+++ src/smart_pointers/interior_mutability.rs
@@ -1,4 +1,6 @@
-pub fn main() {}
+pub fn main() {
+  multiple_owners_of_mutable_data();
+}
 
 pub trait Messenger {
   fn send(&self, msg: &str);
@@ -72,4 +74,30 @@ mod tests {
 
     assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
   }
+}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum List {
+  Cons(Rc<RefCell<i32>>, Rc<List>),
+  Nil,
+}
+
+use List::{Cons, Nil};
+
+fn multiple_owners_of_mutable_data() {
+  let value = Rc::new(RefCell::new(5));
+
+  let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+  let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+  let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+  *value.borrow_mut() += 10;
+
+  println!("a after = {:?}", a);
+  println!("b after = {:?}", b);
+  println!("c after = {:?}", c);
 }
