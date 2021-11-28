a4f6b4cfbef41ad277406bb04d5e61dc5facc2c3
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-11-29 01:47:00 +0200

    Explore multiple producers in thread messages in rust

--- src/concurency/threads.rs
+++ src/concurency/threads.rs
@@ -2,7 +2,8 @@ pub fn main() {
   // thread_basics();
   // move_values_in_thread_closure();
   // message_between_threads();
-  multiple_message();
+  // multiple_message();
+  multiple_producers();
 }
 
 #[allow(dead_code)]
@@ -78,5 +79,47 @@ fn multiple_message() {
 
   for recived in rx {
     println!("Got: {}", recived);
+  }
+}
+
+#[allow(dead_code)]
+fn multiple_producers() {
+  use std::sync::mpsc;
+  use std::thread;
+  use std::time::Duration;
+
+  let (tx, rx) = mpsc::channel();
+
+  let tx1 = tx.clone();
+  thread::spawn(move || {
+    let vals = vec![
+      String::from("hi"),
+      String::from("from"),
+      String::from("the"),
+      String::from("thread"),
+    ];
+
+    for val in vals {
+      tx1.send(val).unwrap();
+      thread::sleep(Duration::from_secs(1));
+    }
+  });
+
+  thread::spawn(move || {
+    let vals = vec![
+      String::from("more"),
+      String::from("messages"),
+      String::from("for"),
+      String::from("you"),
+    ];
+
+    for val in vals {
+      tx.send(val).unwrap();
+      thread::sleep(Duration::from_secs(1));
+    }
+  });
+
+  for revieved in rx {
+    println!("Got: {}", revieved);
   }
 }
