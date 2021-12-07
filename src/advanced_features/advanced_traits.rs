c6418d390ec27b0c103c1ff571be5a68c3b32c5b
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-12-07 23:03:07 +0200

    Explore using supertraits in rust

--- src/advanced_features/advanced_traits.rs
+++ src/advanced_features/advanced_traits.rs
@@ -1,6 +1,7 @@
 pub fn main() {
   operator_overloading();
   advanced_add_output();
+  using_supertraits();
 }
 
 fn operator_overloading() {
@@ -48,4 +49,38 @@ fn advanced_add_output() {
   let met = Meters(1);
 
   assert_eq!(mili + met, Millimeters(1010));
+}
+
+fn using_supertraits() {
+  use std::fmt;
+
+  trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+      let output = self.to_string();
+      let len = output.len();
+
+      println!("{}", "*".repeat(len + 4));
+      println!("*{}*", " ".repeat(len + 2));
+      println!("* {} *", output);
+      println!("*{}*", " ".repeat(len + 2));
+      println!("{}", "*".repeat(len + 4));
+    }
+  }
+
+  struct Point {
+    x: i32,
+    y: i32,
+  }
+
+  impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+  }
+
+  impl OutlinePrint for Point {}
+
+  let point = Point { x: 1, y: 2 };
+
+  point.outline_print();
 }
